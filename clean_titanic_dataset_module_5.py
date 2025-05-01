3b9b871d475658af39b91868b3d8c39cf3eacdb6
Sahil Patel <patelsah@oregonstate.edu>
2025-05-01 12:25:08 -0400

    included requirements.txt

--- clean_titanic_dataset_module_5.py
+++ clean_titanic_dataset_module_5.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 import shutil
 import os 
 from pathlib import Path
-from sklearn.preprocessing import MinMaxScaler
+
+#from sklearn.preprocessing import MinMaxScaler
 
 def print_general_info_for_df(df):
     print("Shape of the DataFrame:",df.shape)
@@ -39,7 +40,8 @@ def print_unique_values_for_all_columns(df):
 
 def clean_titanic_dataset():
     #TODO:create a directory to save plots
-    save_dir = Path("saved_figures")
+    parent_dir = Path(__file__).resolve().parent
+    save_dir = parent_dir / "saved_figures"
     # Create the directory if it does not exist
     save_dir.mkdir(parents=True, exist_ok=True)
     
@@ -68,34 +70,29 @@ def clean_titanic_dataset():
 
 
     #Plotting Fare price for spotting outliers
-    #TODO BEFORE-Fare
-    fare_plot = sns.boxplot(data = df, x ="Fare")
-    plt.xscale("log")
-    plt.title("Distribution of Fare Uncleaned (Log)")
-    plt.show(fare_plot)
-    plt.savefig("Fare_uncleaned.png")
-
+    #TODO CLEAN-Fare
+    print("Fare column distribution(unclean)\n",df['Fare'].describe())
 
     #Remove the rows with huge outliers
     df["Fare"] = pd.to_numeric(df["Fare"], errors = "coerce")
+    df["Fare"] = df["Fare"].astype("float")
     NaNs_in_Fare = df['Fare'].isna().sum()
     print("NaNs in Fare: ", NaNs_in_Fare)  # 0
-    df["Fare"] = df["Fare"].clip(upper=df["Fare"].quantile(0.95))
+    df["Fare"].fillna(df["Fare"].median(), inplace=True)
+    fare_90th_percentile = df["Fare"].quantile(0.75)
+    df["Fare"] = df["Fare"].clip(upper=fare_90th_percentile)
+    df = df[df["Fare"] < 2000]
+    df = df[df["Fare"] > 0]
 
 
-    clean_fare_plot = sns.boxplot(data = df, x = "Fare")
-    plt.xscale("log")
-    plt.title("Distribution of Fare Cleaned (Log)")
-    plt.show(clean_fare_plot)
+    print("Fare column distribution(clean)\n",df['Fare'].describe())
+
+    fare_plot = sns.boxplot(data = df, x ="Fare")
+    plt.xscale("linear")
+    plt.title("Distribution of Fare cleaned (Linear)")
+    #plt.show(fare_plot)
     plt.savefig(f"{save_dir}/Fare_cleaned.png")
 
-    clean_fare_plot = sns.histplot(data=df, x="Fare")
-    plt.xscale("log")
-    plt.title("Distribution of Fare Cleaned (Log)")
-    plt.show(clean_fare_plot)
-    plt.savefig(f"{save_dir}/Fare_cleaned.png")
-
-    print(df['Fare'].describe())
     #Normalize the data
     #scaler = MinMaxScaler()
     #df['Fare'] = scaler.fit_transform(df[['Fare']])
@@ -110,22 +107,17 @@ def clean_titanic_dataset():
 
     #TODO: BEFORE-Age 
 
+    print("Age column distribution(unclean)\n",df['Age'].describe())
 
     plt.figure(figsize=(8, 5))
     sns.histplot(df['Age'], bins=30, kde=True, color="blue")
     plt.title("Age Distribution (uncleaned)")
     plt.xlabel("Age")
     plt.ylabel("Count")
-    plt.show()
+    #plt.show()
+    plt.savefig(f"{save_dir}/age_uncleaned_hist.png")
 
-
-    age_plot = sns.boxplot(data = df, x = "Age")
-    plt.xscale("linear")
-    plt.title("Distribution of Age (Uncleaned)")
-    plt.show(clean_fare_plot)
-    plt.savefig(f"{save_dir}/age_uncleaned.png")
-
-
+    
     #TODO: Clean Age
     #NOTE Replaced the NaN values with the mean Age and Replace the < 1 with 1
     print(df["Age"].describe())
@@ -144,37 +136,37 @@ def clean_titanic_dataset():
     df["Age"] = df["Age"].astype("int")
     #Nobody above 130 should be on a cruise
 
-    df = df[df['Age'] <= 130] 
+    df = df.query("Age < 130") 
+    print("The amount of NaNs in AGE: ",df["Age"].isna().sum())
+    print(df["Age"].describe())
 
     #TODO: AFTER-AGE
+
+    print("Age column distribution(clean)\n",df['Age'].describe())
     plt.figure(figsize=(8, 5))
     sns.histplot(df['Age'], bins=30, kde=True, color="blue")
     plt.title("Age Distribution (Cleaned)")
     plt.xlabel("Age")
     plt.ylabel("Count")
-    plt.show()
+    #plt.show()
+    plt.savefig(f"{save_dir}/age_cleaned_hist.png")
     
-
-    age_plot = sns.boxplot(data = df, x = "Age")
-    plt.xscale("linear")
-    plt.title("Distribution of Age (Cleaned)")
-    plt.show(clean_fare_plot)
-    plt.savefig(f"{save_dir}/age_cleaned.png")
-
+    
     #TODO BEFORE-Pclass
+    print("Pclass column distribution(uclean)\n",df['Pclass'].describe())
     plt.figure(figsize=(8, 5))
     sns.countplot(x='Pclass', hue='Survived', data=df, palette='Set2')
     plt.title("Survival by Passenger Class(Uncleaned)")
     plt.xlabel("Passenger Class")
     plt.ylabel("Count")
-    plt.show()
+    #plt.show()
 
     plt.figure(figsize=(8, 5))
     sns.boxplot(x='Pclass', data=df,)
     plt.title("Survival by Passenger Class(Uncleaned)")
     plt.xlabel("Passenger Class")
     plt.ylabel("Count")
-    plt.show()
+    #plt.show()
     plt.savefig(f"{save_dir}/survival_by_passenger_uncleaned.png")
 
     #TODO: Clean PClass
@@ -189,22 +181,23 @@ def clean_titanic_dataset():
     #df["Pclass"] = df["Pclass"].isin([1,2,3])
 
     #TODO: AFTER-Pclass
-
+    print("Pclass column distribution(clean)\n",df['Pclass'].describe())
     plt.figure(figsize=(8, 5))
     sns.countplot(x='Pclass', hue='Survived', data=df, palette='Set2')
-    plt.title("Survival by Passenger Class(Cleaned)")
+    plt.title("Survival by Passenger Class(Uncleaned)")
     plt.xlabel("Passenger Class")
     plt.ylabel("Count")
-    plt.show()
+    plt.savefig(f"{save_dir}/survival_by_passenger_count_cleaned.png")
 
     plt.figure(figsize=(8, 5))
     sns.boxplot(x='Pclass', data=df,)
     plt.title("Survival by Passenger Class(Cleaned)")
     plt.xlabel("Passenger Class")
     plt.ylabel("Count")
-    plt.show()
-    plt.savefig(f"{save_dir}/survival_by_passenger_cleaned.png")
+    #plt.show()
+    plt.savefig(f"{save_dir}/survival_by_passenger_box_cleaned.png")
 
+    
     #TODO: Other Plots
     ################################################
     #TODO: Fare vs. Survival
@@ -213,31 +206,21 @@ def clean_titanic_dataset():
     plt.title("Scatterplot of Fare vs Survival")
     plt.xlabel("Fare")
     plt.ylabel("Survived (0 = No, 1 = Yes)")
-    plt.show()
+    #plt.show()
     plt.savefig(f"{save_dir}/survival_by_passenger_scatter_cleaned.png")
-
-
-    #TODO: Age vs. Survival
-    plt.figure(figsize=(8, 5))
-    sns.histplot(df['Age'], bins=30, kde=True, color="blue")
-    plt.title("Age Distribution")
-    plt.xlabel("Age")
-    plt.ylabel("Count")
-    plt.show()
-    plt.savefig(f"{save_dir}/Age_Distribution_hist_cleaned.png")
-
-
-
 
     #TODO Survival Count plot
     plt.figure(figsize=(6, 4))
-    sns.countplot(x='Survived', data=df, palette='coolwarm')
+    sns.countplot(x='Survived', data=df)
     plt.title("Survival Count")
     plt.xlabel("Survived (0 = No, 1 = Yes)")
     plt.ylabel("Count")
-    plt.show()
+    #plt.show()
     plt.savefig(f"{save_dir}/Survival_countplot_cleaned.png")
     ##################################################
     
+    df.to_csv("Titanic_Kaggle_Train_Data_Cleaned.csv", index=False)
+    print("Cleaned DataFrame saved to Titanic_Kaggle_Train_Data_Cleaned.csv")
+
 if __name__ == "__main__":
     clean_titanic_dataset()
